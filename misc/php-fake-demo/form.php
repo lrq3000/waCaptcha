e46b2e954e11a0009e8647a263aedcddce62843d
Stephen L <lrq3000@gmail.com>
2013-05-24 22:39:56 +0200

    fix: moved index.php -> form.php
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- misc/php-fake-demo/form.php
+++ misc/php-fake-demo/form.php
@@ -1,63 +1,65 @@
 <?php
+// Include the config file
+include(dirname(__FILE__).'/config.php');
+
+// Start the session
 session_start();
+// Get the option and set the session
 if (!empty($_GET['option'])) {
     $_SESSION['option'] = $_GET['option'];
 }
 
-$options = array(
-                                't1' => 'Default - 4 objects, 320x240, model+name',
-                                't2' => 'More objects - 10 objects, 320x240, model+name',
-                                't3' => 'Bigger resolution - 4 objects, 800x600, model+name',
-                                't4' => 'Harder mode - 4 objects, 320x240, model only',
-                                't5' => 'Hardest mode - 4 objects, 320x240, name only',
-                            );
-
-if (empty($_SESSION['option'])) {
+if (isset($options[$_SESSION['option']])) {
+    // If session is set and valid, we use it
+    $cpath = $_SESSION['option'];
+} else {
+    // Else we load the images from the first images folder
     $tmp = array_keys($options);
-    $cpath = 'solmasks'.$tmp[0];
+    $cpath = reset($tmp);
     unset($tmp);
-} else {
-    $cpath = 'solmasks'.$_SESSION['option'];
 }
-$solpath = $cpath;
+$solpath = $cpath; // here in this demo application, the cpath (path to checking images) and solpath (path to solution images) are the same since we don't need any security
 
-if (!empty($_SESSION['option']) and !strcmp($_SESSION['option'], 't3')) {
-    $width = 800;
-    $height = 600;
-} else {
-    $width = 320;
-    $height = 240;
-}
 
 /****************************/
 
+// Include the required captcha validation processing libraries
 include(dirname(__FILE__).'/formlib.php');
 include(dirname(__FILE__).'/rlereaderlib.php');
 
+// Get the user's input if submitted
 if (!empty($_POST['cid'])) $cid = $_POST['cid']; else $cid = '';
 if (!empty($_POST['csol'])) $csol = $_POST['csol']; else $csol = '';
 if (!empty($_POST['nojs_csol_x'])) $nojs_csol_x = $_POST['nojs_csol_x']; else $nojs_csol_x = '';
 if (!empty($_POST['nojs_csol_y'])) $nojs_csol_y = $_POST['nojs_csol_y']; else $nojs_csol_y = '';
 
+ // Check the user's input if submitted
 if (!empty($cid)) {
+    // Extract the coordinates
     if (!empty($csol) and strcmp($csol, 'NULL')) {
+        // Javascript input, the coordinates are concatenated like : xxx:yyy
         list($x,$y) = explode(':', $csol);
     } else {
+        // HTML input, the coordinates are separated into 2 different fields
         $x = $nojs_csol_x;
         $y = $nojs_csol_y;
     }
 
-    if (checkPixel($solpath, $cid, $x, $y)) {
+    // Check the solution
+    if (checkPixel($solpath, $cid, $x, $y)) { // OK, correct
         $cresult = '<span style="font-weight: bold; color: darkgreen;">VALID: Captcha is CORRECT!</span>';
-    } else {
+    } else { // KO, wrong
         $cresult = '<span style="font-weight: bold; color: red;">INVALID: Captcha is incorrect...</span>';
     }
 }
 
+// Get a random image id
 $cid = getRandomImage($cpath);
 
-$form = getform($cpath, $solpath, $cid, $width, $height);
+// Show the image and the dynamical input form
+$form = getform($cpath, $solpath, $cid);
 
+// The rest is the skeleton of the HTML page
 ?>
 
 <!DOCTYPE HTML>
@@ -113,6 +115,6 @@ $form = getform($cpath, $solpath, $cid, $width, $height);
         <input type="submit" value="Submit" />
     </form>
     <hr />
-    <p>Description: This captcha is very alike the Where's Waldo game: you get a reference object (placed randomly at the right or left of the image), and you must find it inside the other image. The software ensures that there's always a minimum clickable area for the solution, and that it is also not too big. Colours and placement are totally randomized. The reference model is unique in the scene, there cannot be a duplicate. The form use Javascript if available, but if not the form is totally HTML compliant so you can just disable Javascript and keep validating captchas.<br /></p>
+    <p>Description: This captcha is very alike the Where's Waldo game: you get a reference object (placed randomly at the right or left of the image), and you must find it inside the other image. The software ensures that there's always a minimum clickable area for the solution, and that it is also not too big. Colours and placement are totally randomized. The reference model is unique in the scene, there cannot be a duplicate. The form use Javascript if available, but if not the form is totally HTML compliant so you can just disable Javascript and keep validating captchas. Another interesting usage is for the performance assessment of object recognition softwares, since the API is easy to use and totally automated.<br /></p>
   </body>
 </html>
